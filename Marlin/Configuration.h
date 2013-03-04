0bceb0746be6fa21445c7b10ebdd890a2be88127
Erik Kastner <kastner@gmail.com>
2013-03-04 09:51:50 -0500

    tweaks, bot works, leveling does not

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -62,25 +62,29 @@
 // #define DELTA_DIAGONAL_ROD 287.0 // mm actual? - this made the model big on x/y, small on z
 // #define DELTA_DIAGONAL_ROD 295.0 // mm - 58.61
 // #define DELTA_DIAGONAL_ROD 288.2 // mm - 50.55
-#define DELTA_DIAGONAL_ROD 290.0 // REAL CLOSE... sticking with this for printing out the replacement carriage
+// #define DELTA_DIAGONAL_ROD 290.0 // - 59.90 x 50.0 x 3.5 REAL CLOSE... sticking with this for printing out the replacement carriage
+#define DELTA_DIAGONAL_ROD 288.5
 
 // Horizontal offset from middle of printer to smooth rod center.
 #define DELTA_SMOOTH_ROD_OFFSET 206.0 // mm
 
 // Horizontal offset of the universal joints on the end effector.
-#define DELTA_EFFECTOR_OFFSET 32.0 // mm
+// #define DELTA_EFFECTOR_OFFSET 32.0 // mm
+#define DELTA_EFFECTOR_OFFSET 42.0 // mm
 
 // Horizontal offset of the universal joints on the carriages.
-#define DELTA_CARRIAGE_OFFSET 26.0 // mm
+// #define DELTA_CARRIAGE_OFFSET 26.0 // mm
+#define DELTA_CARRIAGE_OFFSET 25.0 // mm
 
 // In order to correct low-center, DELTA_RADIUS must be increased.
 // In order to correct high-center, DELTA_RADIUS must be decreased.
 // For convex/concave -- -20->-30 makes the center go DOWN
 // #define DELTA_FUDGE -27.4 // 152.4 total radius
-#define DELTA_FUDGE -4.5
+#define DELTA_FUDGE -6.3
 
 // Effective horizontal distance bridged by diagonal push rods.
 #define DELTA_RADIUS (DELTA_SMOOTH_ROD_OFFSET-DELTA_EFFECTOR_OFFSET-DELTA_CARRIAGE_OFFSET-DELTA_FUDGE)
+// #define DELTA_RADIUS (DELTA_SMOOTH_ROD_OFFSET-DELTA_EFFECTOR_OFFSET-DELTA_CARRIAGE_OFFSET-DELTA_FUDGE)
 
 // Effective X/Y positions of the three vertical towers.
 #define SIN_60 0.8660254037844386
@@ -299,10 +303,10 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 #define min_software_endstops true //If true, axis won't move to coordinates less than *_MIN_POS.
 #define max_software_endstops true //If true, axis won't move to coordinates greater than *_MAX_POS.
 
-#define X_MAX_POS 90
-#define X_MIN_POS -90
-#define Y_MAX_POS 90
-#define Y_MIN_POS -90
+#define X_MAX_POS 110
+#define X_MIN_POS -110
+#define Y_MAX_POS 110
+#define Y_MIN_POS -110
 #define Z_MAX_POS MANUAL_Z_HOME_POS
 #define Z_MIN_POS 0
 
@@ -329,11 +333,12 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 // #define MANUAL_Z_HOME_POS 195.37  // (j-head - glass -- small -- mounted under)
 //#define MANUAL_Z_HOME_POS 165.37  // (j-head - bigglass -- long arms)
 // #define MANUAL_Z_HOME_POS 165.5  // (j-head - bigglass -- long arms)
-#define MANUAL_Z_HOME_POS 165.5  // (j-head - bigglass -- long arms)
+// #define MANUAL_Z_HOME_POS 165.6  // (j-head - bigglass -- long arms)
+#define MANUAL_Z_HOME_POS 166.7 // 166.7
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
-#define HOMING_FEEDRATE {200*60, 200*60, 200*60, 0}  // set the homing speeds (mm/min)
+#define HOMING_FEEDRATE {500*60, 500*60, 500*60, 0}  // set the homing speeds (mm/min)
 
 // default settings 
 
@@ -344,11 +349,11 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {DELTA_AXIS_STEPS, DELTA_AXIS_STEPS, DELTA_AXIS_STEPS, 96.8} // 1.75mm airtripper
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {DELTA_AXIS_STEPS, DELTA_AXIS_STEPS, DELTA_AXIS_STEPS, 101.30} // 3mm greg's / printrbot
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {DELTA_AXIS_STEPS, DELTA_AXIS_STEPS, DELTA_AXIS_STEPS, 554.00} // 1.75mm greg's / printrbot
-#define DEFAULT_MAX_FEEDRATE          {400, 400, 400, 20}  // (mm/sec)
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 500, 60}  // (mm/sec)
 
-#define DEFAULT_MAX_ACCELERATION      {400, 400, 400, 400}    // X, Y, Z, E maximum start speed for accelerated moves.
+#define DEFAULT_MAX_ACCELERATION      {2000, 2000, 2000, 400}    // X, Y, Z, E maximum start speed for accelerated moves.
 
-#define DEFAULT_ACCELERATION          400   // X, Y, Z and E max acceleration in mm/s^2 for printing moves
+#define DEFAULT_ACCELERATION          600   // X, Y, Z and E max acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  400   // X, Y, Z and E max acceleration in mm/s^2 for r retracts
 
 // 
